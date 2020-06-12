1e50ba3f5ee12e22a8ffbd886b4b9c2840497664
Arif Cagirici <acagirici23@gmail.com>
2020-06-12 16:12:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,5 +39,12 @@ class Song
     
   def self.artist_count
     artist_count = {}
-    
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count[artist] = 1 
+      end 
+       return artist_count
+      end
 end
