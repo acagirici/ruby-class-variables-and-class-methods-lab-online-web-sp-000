7359d17f280c93b008dee5c3ea62175410506778
Arif Cagirici <acagirici23@gmail.com>
2020-06-12 16:00:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,5 +28,11 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      
+      if genre_count|genre|
+        genre_count += 1
+      else
+        genre_count = 1
+      end
+      return genre_count
+    end
 end
